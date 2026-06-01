dae4986ba32ecd221ebbedf1182b550acd703612
*vivek <vrvivekofficial@gmail.com>
2026-06-01 17:43:58 +0530

    changed position of frame as objects section

--- app/page.tsx
+++ app/page.tsx
@@ -19,10 +19,10 @@ export default function Home() {
       <main>
         <HeroSection />
         <BrandPhilosophy />
+        <FeaturedFrames />
         <SignatureCollections />
         <CraftsmanshipStory />
         <HorizontalGallery />
-        <FeaturedFrames />
         <BrandManifesto />
         <FeaturedCampaign />
         <JournalPreview />
